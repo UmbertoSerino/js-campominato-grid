9fc231cd94399a8bf79c48c7296478d85a8071cb
UmbertoSerino <serumberto@gmail.com>
2023-11-21 16:18:19 +0100

    created function to gen squares

--- script/script.js
+++ script/script.js
@@ -1,12 +1,36 @@
 // Consegna:
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-bottone ok
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+
+
 // Bonus
 // Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 // creata select con diversi 3 tipi di difficolta'
 // con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+
+
+
+
+
+
+
+
+// ========== Function ==========
+/**
+ * funzione per creare una cella!
+ */
+function createdSquare() {
+    const squareElement = document.createElement ("div");
+    squareElement = document.classList ("square-class");
+    return squareElement;    
+}
+
+
+

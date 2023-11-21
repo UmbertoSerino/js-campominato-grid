9b2b2ee3c2798c742cc4fe7b74c684fe5637d903
UmbertoSerino <serumberto@gmail.com>
2023-11-21 16:56:20 +0100

    created ciclo for

--- script/script.js
+++ script/script.js
@@ -1,7 +1,24 @@
 // Consegna:
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
+const buttonGames = document.querySelector (".button-start");
+console.log(buttonGames);
+const containerBig = document.querySelector("article.container-grid");
+console.log(containerBig);
+
+
+
+
+
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+
+for (let index = 0; index < 100; index++) {
+    containerBig.appendChild(createdSquare());
+    
+}
+
+
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 
@@ -14,21 +31,13 @@
 // con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
-
-
-
-
-
-
-
-
 // ========== Function ==========
 /**
  * funzione per creare una cella!
  */
 function createdSquare() {
     const squareElement = document.createElement ("div");
-    squareElement = document.classList ("square-class");
+    squareElement.classList.add ("square-class");
     return squareElement;    
 }
 
